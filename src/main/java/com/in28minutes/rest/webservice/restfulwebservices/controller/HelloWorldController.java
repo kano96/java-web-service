8d0049869c27096d302c33515eafbba1d63a7f23
Kevin Torres <ketorres@pichincha.com>
2024-01-24 15:32:06 -0500

    add internationalized messages

--- src/main/java/com/in28minutes/rest/webservice/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservice/restfulwebservices/controller/HelloWorldController.java
@@ -1,24 +1,42 @@
 package com.in28minutes.rest.webservice.restfulwebservices.controller;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World!";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
 
     @GetMapping("/hello-world/path-variable/{name}")
-    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+
         return new HelloWorldBean(String.format("Hello World, %s ", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
     }
 }
--- src/main/java/com/in28minutes/rest/webservice/restfulwebservices/error/ErrorDetails.java
+++ src/main/java/com/in28minutes/rest/webservice/restfulwebservices/error/ErrorDetails.java
@@ -2,26 +2,5 @@ package com.in28minutes.rest.webservice.restfulwebservices.error;
 
 import java.time.LocalDate;
 
-public class ErrorDetails {
-    private LocalDate timestamp;
-    private String message;
-    private String details;
-
-    public ErrorDetails(LocalDate timestamp, String message, String details) {
-        this.timestamp = timestamp;
-        this.message = message;
-        this.details = details;
-    }
-
-    public LocalDate getTimestamp() {
-        return timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getDetails() {
-        return details;
-    }
+public record ErrorDetails(LocalDate timestamp, String message, String details) {
 }
